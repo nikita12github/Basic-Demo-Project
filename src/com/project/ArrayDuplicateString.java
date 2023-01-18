d47506bb0ff6e860b568c4820b64c3c814a845ca
nikita <jadhavniku5o@gmail.com>
2023-01-18 14:55:45 +0530

    Array basic demo

---
+++ src/com/project/ArrayDuplicateString.java
@@ -0,0 +1,16 @@
+package com.project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDuplicateString {
+	   
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		list.add("java");
+		list.add("Hibernate");
+		list.add("Springboot");
+		System.out.println(list);
+	}
+
+}
